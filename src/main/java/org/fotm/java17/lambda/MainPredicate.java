92ffeea4e22dff68804eb1c6949b0fe400c81570
kxen42 <kxen42@users.noreply.github.com>
2023-03-15 20:26:28 -0500

    Predicate Experiments

--- src/main/java/org/fotm/java17/lambda/MainPredicate.java
+++ src/main/java/org/fotm/java17/lambda/MainPredicate.java
@@ -1,5 +1,6 @@
 package org.fotm.java17.lambda;
 
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 interface Evaluate<T> {
@@ -26,10 +27,21 @@ public class MainPredicate {
         // the magic of Generics
         System.out.println(check(42, i -> i % 2 == 0));
         System.out.println(check("boo", s -> s.contains("o")));
+
+        // BiPredicate
+        BiPredicate<String, Integer> hasLength = (str, len) -> str.length() == len;
+        System.out.println(hasLength.test("Fred", 3));
+
+        System.out.println(check("Fred", "Wilma", (s1, s2) -> s1.contains(s2)));
     }
 
     // Passing a Predicate / lambda as a method argument
     public static <T> boolean check(T t, Predicate<T> lambda) {
         return lambda.test(t);
+    }
+
+    // Passing a BiPredicate / lambda as a method argument
+    public static <T, U> boolean check(T t, U u, BiPredicate<T, U> lambda) {
+        return lambda.test(t, u);
     }
 }
