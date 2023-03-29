bed394aca7531935e91d89f672a91884cfbe3510
kxen42 <kxen42@users.noreply.github.com>
2023-03-29 16:55:56 -0500

    Sealed classes and interfaces

---
+++ src/main/java/org/fotm/java17/misc/SealedClasses.java
@@ -0,0 +1,39 @@
+package org.fotm.java17.misc;
+
+public class SealedClasses {
+
+    sealed interface Driveable permits Vehicle, Truck, Golfcart, Scooter {
+
+    }
+
+    /*
+    if you implement a sealed interface the class must be marked with one of
+    sealed, non-sealed, final
+    */
+    public sealed class Vehicle implements Driveable permits Car {}
+
+    public sealed class Car extends Vehicle permits Ford, Volvo, Audi{}
+
+    final class Ford extends Car {}
+    final class Volvo extends Car {}
+    non-sealed class Audi extends Car {}
+
+    // no surprise you can extend a non-sealed class
+    class RedAudio extends Audi {}
+
+
+    // a sealed class MUST have subclasses
+    public sealed class Scooter implements Driveable{}
+    public final class Honda extends Scooter {}
+
+    // non-sealed class doesn't need subclass
+    public non-sealed class Truck implements Driveable {}
+
+    // no suprise that final class still doesn't need subclass
+    // even if it implements a sealed interface
+    public final class Golfcart implements Driveable {}
+
+    public sealed interface Cartoon permits Flinstones{}
+    public non-sealed interface Flinstones extends Cartoon{}
+
+}
