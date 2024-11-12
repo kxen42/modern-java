5807f60fce59e65aad163a3bb766395038007312
kxen42 <kxen42@users.noreply.github.com>
2024-11-12 16:09:01 -0600

    Optionals

---
+++ src/main/java/org/fotm/java8/OptionalExamples.java
@@ -0,0 +1,23 @@
+package org.fotm.java8;
+
+import java.util.Optional;
+
+public class OptionalExamples {
+    public static void main(String[] args) {
+        calculateAverageWithoutStream();
+    }
+
+    public static Optional<Double> calculateAverageWithoutStream(int... input) {
+        System.out.println(" ----- calculateAverageWithoutStream");
+        if (input.length == 0) return Optional.empty();
+        int sum = 0;
+        for (int i : input) sum += i;
+        return Optional.of((double) sum / input.length);
+
+    }
+
+    public static Optional<String> handlePossibleNullValue(String str) {
+        // str == null ? Optional.empty() : Optional.of(str)
+        return Optional.ofNullable(str);
+    }
+}
---
+++ src/test/java/org/fotm/java8/OptionalExamplesTest.java
@@ -0,0 +1,90 @@
+package org.fotm.java8;
+
+import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+
+
+class OptionalExamplesTest {
+
+    @Test
+    void calculateAverageWithoutStream_Empty() {
+        Optional<Double> v = OptionalExamples.calculateAverageWithoutStream();
+        assertThat(v).isEmpty();
+    }
+
+    @Test
+    void calculateAverageWithoutStream_OneArg() {
+        Optional<Double> v = OptionalExamples.calculateAverageWithoutStream(42);
+        assertThat(v).isPresent();
+        assertThat(v).hasValue(42.0);
+    }
+
+    @Test
+    void calculateAverageWithoutStream_ManyArgs() {
+        Optional<Double> v = OptionalExamples.calculateAverageWithoutStream(1, 2, 42, 532, 47, 4021, 1166);
+        assertThat(v).isPresent();
+        assertThat(v).hasValue(830.1428571428571);
+    }
+
+    @Test
+    void calculateAverageWithEmptyStream_NaN() {
+        Optional<Double> v = OptionalExamples.calculateAverageWithoutStream();
+        assertThat(Double.isNaN(v.orElse(Double.NaN))).isTrue();
+    }
+
+    @Test
+    void calculateAverageWithEmptyStream_NotNaN() {
+        Optional<Double> v = OptionalExamples.calculateAverageWithoutStream(10, 20, 30);
+        assertThat(Double.isNaN(v.orElse(Double.NaN))).isFalse();
+    }
+
+    @Test
+    void calculateAverageWithEmptyStream_OrElseGet() {
+        Optional<Double> v = OptionalExamples.calculateAverageWithoutStream(10, 20, 30);
+        assertThat(Double.isNaN(v.orElseGet(Math::random))).isFalse();
+    }
+
+    @Test
+    void handlePossibleNullValue_NotNull() {
+        Optional<String> v = OptionalExamples.handlePossibleNullValue("Bob");
+        assertThat(v).isPresent()
+                     .hasValue("Bob");
+    }
+
+    @Test
+    void handlePossibleNullValue_Null() {
+        Optional<String> v = OptionalExamples.handlePossibleNullValue(null);
+        assertThat(v).isNotPresent()
+                     .isEmpty();
+
+        String result = v.orElseGet(() -> "ouch");
+        assertThat(result).isEqualTo("ouch");
+
+        String result2 = v.orElse("boo");
+        assertThat(result2).isEqualTo("boo");
+
+        assertThatIllegalStateException().isThrownBy(() -> {
+            String s = v.orElseThrow(IllegalStateException::new);
+        });
+    }
+
+    @Test
+    void primitiveAverage() {
+        IntStream range = IntStream.range(1, 5);
+        OptionalDouble average = range.average();
+
+        assertThat(average).hasValueCloseTo(2.0, Offset.offset(0.5));
+
+        range = IntStream.of();
+        average = range.average();
+
+        assertThat(average).isEmpty();
+    }
+}
