752657b8a90caea466c9b68a266c86d5648af845
kxen42 <kxen42@users.noreply.github.com>
2024-11-14 17:56:13 -0600

    Consumer andThen.andThen.accept

--- src/main/java/org/fotm/java8/consumer/DoingStuffWithConsumer.java
+++ src/main/java/org/fotm/java8/consumer/DoingStuffWithConsumer.java
@@ -11,22 +11,34 @@ public class DoingStuffWithConsumer {
     public static void main(String[] args) {
         List<Student> students = StudentGenerator.createMultipleStudents();
 
-        Consumer<Student> printStudent = System.out::println;
-        Consumer<Student> printName = s -> System.out.println(s.getName()
-                                                               .toUpperCase());
-
-        Consumer<Student> printActivities = s -> System.out.println(String.join(", ", s.getActivities()));
+        Consumer<Student> studentConsumer = System.out::println;
+        Consumer<Student> nameConsumer = s -> System.out.println(s.getName()
+                                                                  .toUpperCase());
+        Consumer<Student> gradeConsumer = s -> System.out.print("grade: " + s.getGrade() + ", activities: ");
+        Consumer<Student> activitiesConsumer = s -> System.out.println(String.join(", ", s.getActivities()));
 
         System.out.println(" ----- Print students");
-        students.forEach(printStudent);
+        students.forEach(studentConsumer);
 
         System.out.println(" ----- Print names and activities");
-        students.forEach(printName.andThen(printActivities)); // consumer chaining
+        students.forEach(nameConsumer.andThen(activitiesConsumer)); // consumer chaining
 
         System.out.println(" ----- Alternate way to print names and activities ");
         students.forEach(
-            s -> printName.andThen(printActivities) // more consumer chaining
-                          .accept(s) // this kicks off the chain of consumer's like Hungarian notation
+            s -> nameConsumer.andThen(activitiesConsumer) // more consumer chaining
+                             .accept(s) // this kicks off the chain of consumer's like Hungarian notation
         );
+
+        System.out.println(" ----- Print the A and B students activities");
+        students.forEach(s -> {
+            if (s.getGrade()
+                 .equals(Student.Grade.A) || s.getGrade()
+                                              .equals(Student.Grade.B)) {
+                // The syntax makes it easy to forget that this is running on a Student
+                nameConsumer.andThen(gradeConsumer)
+                            .andThen(activitiesConsumer)
+                            .accept(s);
+            }
+        });
     }
 }
