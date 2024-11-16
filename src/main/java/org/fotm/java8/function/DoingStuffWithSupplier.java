8f36cb1367de6a5c124168b6bc04ed029da9e948
kxen42 <kxen42@users.noreply.github.com>
2024-11-15 22:31:56 -0600

    DoingStuffWithSupplier

--- src/main/java/org/fotm/java8/function/DoingStuffWithSupplier.java
+++ src/main/java/org/fotm/java8/function/DoingStuffWithSupplier.java
@@ -3,6 +3,7 @@ package org.fotm.java8.function;
 import org.fotm.model.Car;
 import org.fotm.model.CarGenerator;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -13,9 +14,12 @@ public class DoingStuffWithSupplier {
         // Things are more interesting with Stream.collect()
         Supplier<List<Car>> cars = CarGenerator::createCars;
         Supplier<Car> car = () -> Car.builder().build();
+        Supplier<List<Car>> newList = ArrayList::new;
 
         System.out.println(cars.get());
         System.out.println("-------------------------");
         System.out.println(car.get());
+        System.out.println("-------------------------");
+        System.out.println(newList.get());
     }
 }
