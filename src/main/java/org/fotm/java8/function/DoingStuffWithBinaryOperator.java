ae4e56e3fac43704075cd21fa58e722ca21647fe
kxen42 <kxen42@users.noreply.github.com>
2024-11-15 22:16:06 -0600

    DoingStuffWithBinaryOperator more complicated

--- src/main/java/org/fotm/java8/function/DoingStuffWithBinaryOperator.java
+++ src/main/java/org/fotm/java8/function/DoingStuffWithBinaryOperator.java
@@ -1,13 +1,33 @@
 package org.fotm.java8.function;
 
+import java.util.Comparator;
 import java.util.function.BinaryOperator;
 
 public class DoingStuffWithBinaryOperator {
 
     private static final BinaryOperator<Double> tangent = (x, y) -> Math.sin(x) / Math.cos(y);
 
+    private static final Comparator<String> compareByLength = Comparator.comparingInt(String::length);
+
+    private static final BinaryOperator<String> concatenated = String::concat;
+
     public static void main(String[] args) {
 
         System.out.println("tangent x & y: " + tangent.apply(Math.PI, 2.0));
+
+        BinaryOperator<String> maxByOperator = BinaryOperator.maxBy(compareByLength);
+        String maxBy = maxByOperator.apply("boo", "ouch");
+
+        System.out.println(maxBy);
+
+        BinaryOperator<String> minByOperator = BinaryOperator.minBy(compareByLength);
+        String minBy = minByOperator.apply("boo", "ouch");
+
+        System.out.println(minBy);
+
+        String mergedAndThenToUpperCase = concatenated.andThen(String::toUpperCase)
+                                                      .apply("boo", "ouch");
+
+        System.out.println(mergedAndThenToUpperCase);
     }
 }
