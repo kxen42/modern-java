4d0a8209a3354bd0b98dac7d36fa1ed401aec187
kxen42 <kxen42@users.noreply.github.com>
2024-11-19 17:08:09 -0600

    TODO: exercises for Stream methods since Java 8

---
+++ src/main/java/org/fotm/java17/stream/StreamMethodsAddedAfterJava8.java
@@ -0,0 +1,19 @@
+package org.fotm.java17.stream;
+
+/**
+ * TODO:
+ * dropWhile<br>
+ * iterate<br>
+ * mapMulti<br>
+ * mapMultiToDouble<br>
+ * mapMultiToInt<br>
+ * mapMultiToLong<br>
+ * ofNullable<br>
+ * takeWhile<br>
+ * toList<br>
+ */
+public class StreamMethodsAddedAfterJava8 {
+    public static void main(String[] args) {
+
+    }
+}
