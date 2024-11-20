55842f618c606c7a1d75f4e563c954e6b41e6013
kxen42 <kxen42@users.noreply.github.com>
2024-11-20 15:18:52 -0600

    More flatMap

--- src/main/java/org/fotm/stream/FlatMap.java
+++ src/main/java/org/fotm/stream/FlatMap.java
@@ -1,5 +1,7 @@
 package org.fotm.stream;
 
+import org.fotm.model.Customer;
+import org.fotm.model.CustomerGenerator;
 import org.fotm.model.Member;
 import org.fotm.model.MemberGenerator;
 
@@ -15,21 +17,45 @@ import java.util.stream.Stream;
 public class FlatMap {
 
     private static final List<Member> members = MemberGenerator.createMembers();
+    private static final List<Customer> customers = CustomerGenerator.createCustomers();
 
     public static void main(String[] args) {
-        getCourseNamesForMemebers();
+        getCourseNamesForMembers();
         getWordsFromLinesInFile();
         streamOfLists();
+        getOrdersForAllCustomers();
     }
 
-    private static void getCourseNamesForMemebers() {
+    private static void getCourseNamesForMembers() {
+        System.out.println(" ----- getCourseNamesForMembers");
+        // Member.courses is List<String>
         members.stream()
                .flatMap(m -> m.getCourses()
                               .stream())
                .forEach(System.out::println);
     }
 
+    public static void getOrdersForAllCustomers() {
+        System.out.println(" ----- getOrdersForAllCustomers");
+        // Customer.order is a Integer[]
+        customers.stream()
+                 .map(Customer::getOrders)
+                 .flatMap(Stream::of)
+                 .distinct()
+                 .sorted()
+                 .forEach(System.out::println);
+
+        System.out.println(" ----- getOrdersForAllCustomers alternate syntax");
+        customers.stream()
+                 .flatMap(c -> Stream.of(c.getOrders()))
+                 .distinct()
+                 .sorted()
+                 .forEach(System.out::println);
+
+    }
+
     private static void getWordsFromLinesInFile() {
+        System.out.println(" ----- getWordsFromLinesInFile");
         try (Stream<String> lines = Files.lines(Path.of("src/main/resources/ZombieIpsum.txt"))) {
             lines.flatMap(line ->
                               Stream.of(line.split("[ \n\t.?,]+")))
@@ -38,15 +64,17 @@ public class FlatMap {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        System.out.println();
     }
-    
+
     private static void streamOfLists() {
+        System.out.println(" ----- streamOfLists");
         List<String> flintstones = List.of("Fred", "Wilma");
         List<String> bradys = List.of("Greg", "Peter", "Bobby", "Cindy", "Jan", "Marcia");
         List<String> castaways = List.of("Skipper", "Gilligan");
 
         Stream.of(flintstones, bradys, castaways) // this creates a stream of lists
-            .flatMap(Collection::stream) // this runs stream on each list to get a list of names
-            .forEach(System.out::println);  // spew names
+              .flatMap(Collection::stream) // this runs stream on each list to get a list of names
+              .forEach(System.out::println);  // spew names
     }
 }
