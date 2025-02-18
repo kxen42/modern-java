f073e3d7e80f8511fd5155427e63431920549b9e
kxen42 <kxen42@users.noreply.github.com>
2025-02-18 17:28:33 -0600

    Things you can do in Java 21

---
+++ src/main/java/org/fotm/java21/misc/SwitchExpression21.java
@@ -0,0 +1,21 @@
+package org.fotm.java21.misc;
+
+import org.fotm.java17.interfaces.Circle;
+import org.fotm.java17.interfaces.Shape;
+import org.fotm.java17.interfaces.Square;
+
+public class SwitchExpression21 {
+    public static void main(String[] args) {
+        sealedClass(new Circle());
+        sealedClass(new Square());
+    }
+
+    public static void sealedClass(Shape shape) {
+        System.out.println(" ----- sealedClass");
+        var result = switch(shape) {
+            case Circle c -> c.name() + "K";
+            case Square sq -> sq.name()  + "pants";
+        };
+        System.out.println("Look what I can do " + result);
+    }
+}
